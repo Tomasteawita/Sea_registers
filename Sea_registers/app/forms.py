21c1ed0f79e6cf9434898dae136b0d9ff81acd89
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2023-06-03 08:27:54 -0300

    Index's base already created and forms were add.

--- Sea_registers/app/forms.py
+++ Sea_registers/app/forms.py
@@ -1,7 +1,26 @@
 from django import forms
+from .models import *
 from django.contrib.auth.models import  User
 from django.contrib.auth.forms import UserCreationForm
 
+
+class TeacherForm(forms.Form):
+    user = forms.ModelChoiceField(queryset = User.objects.all())
+    name = forms.CharField(max_length = 50, required = False)
+    phone = forms.CharField(max_length = 12)
+    image = forms.ImageField()
+    
+class SchoolForm(forms.Form):
+    name = forms.CharField(max_length = 150)
+
+class CommissionForm(forms.Form):
+    name = forms.CharField(max_length= 3)
+    school = forms.ModelChoiceField(queryset = School.objects.all())
+    teacher = forms.ModelChoiceField(queryset = Teacher.objects.all())
+
+class StudentForm(forms.Form):
+    name = forms.CharField(max_length= 150)
+    comission = forms.ModelChoiceField(queryset = Commission.objects.all())
 
 class SingUpForm(UserCreationForm):
     class Meta:
--- Sea_registers/app/models.py
+++ Sea_registers/app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Teacher(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     name = models.CharField(max_length = 50)
-    phone = models.CharField(max_length = 50) 
+    phone = models.CharField(max_length = 12) 
     image = models.ImageField(upload_to = 'perfil_image',null = True,blank = True)
     def __str__(self) -> str:
         return  f'{super().__str__()}' 
@@ -18,7 +18,11 @@ class Commission(models.Model):
     name = models.CharField(max_length = 3)
     school = models.ForeignKey(School, on_delete = models.CASCADE)
     teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
-
+    
+    def get_school(self, school_template_id):
+        school = School.objects.get(id = school_template_id)
+        return f'{school}'
+        
 class Student(models.Model):
     name = models.CharField(max_length = 150)
     comission = models.ForeignKey(Commission, on_delete = models.CASCADE)
